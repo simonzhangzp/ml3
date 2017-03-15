2edc16d8817ea10622c96142fdb5c201c2bad358
seladore <mbdesai@umich.edu>
2017-03-15 10:55:51 -0400

    adjustments

--- random_forest.r
+++ random_forest.r
@@ -8,20 +8,20 @@ library(randomForest)
 
 # used random seed of 11 and 80% in training set
 set.seed(11)
-customers = read.csv("data.csv", header=TRUE)
-missing_customers = customers[rowSums(is.na(customers)) > 0,]
-customers = na.exclude(customers)
-#missing_customers = customers[is.na(customers)]
+all_customers = read.csv("data.csv", header=TRUE)
+all_train = sample(1:nrow(all_customers), nrow(all_customers)*0.8)
+all_valid = all_customers[-all_train,"Churn"]
+missing_customers = all_customers[rowSums(is.na(all_customers)) > 0,]
+customers = na.exclude(all_customers)
 train = sample(1:nrow(customers), nrow(customers)*0.8)
-customers_valid = customers[-train,"Churn"]
+valid = customers[-train,"Churn"]
 
 
 # random forest with all predictors
-#x_vars = length(customers) - 2
 #str(customers)
 rf_customers = randomForest(Churn~.-customerID, data=customers, subset=train , mtry=19, importance=TRUE)
 rf_predict = predict(rf_customers, newdata=customers[-train,])
-rf_error = mean(rf_predict!=customers_valid)
+rf_error = mean(rf_predict!=valid)
 # 0.2137531
 importance(rf_customers)
 varImpPlot(rf_customers)
@@ -29,7 +29,7 @@ varImpPlot(rf_customers)
 # random forest with important predictors
 rf_customers = randomForest(Churn~Contract+tenure+TotalCharges+MonthlyCharges+OnlineSecurity, data=customers, subset=train , mtry=4, importance=TRUE)
 rf_predict = predict(rf_customers, newdata=customers[-train,])
-rf_error = mean(rf_predict!=customers_valid)
+rf_error = mean(rf_predict!=valid)
 # 0.222866
 importance(rf_customers)
 varImpPlot(rf_customers)
@@ -37,9 +37,40 @@ varImpPlot(rf_customers)
 # increase trees
 rf_customers = randomForest(Churn~.-customerID, data=customers, subset=train , mtry=19, importance=TRUE, ntrees=1000)
 rf_predict = predict(rf_customers, newdata=customers[-train,])
-rf_error = mean(rf_predict!=customers_valid)
+rf_error = mean(rf_predict!=valid)
 # 0.211267
 importance(rf_customers)
 varImpPlot(rf_customers)
 
-# 
+# increase trees less vars
+rf_customers = randomForest(Churn~Contract+MonthlyCharges+SeniorCitizen+tenure+Dependents, data=customers, subset=train , mtry=5, importance=TRUE)
+rf_predict = predict(rf_customers, newdata=customers[-train,])
+rf_error = mean(rf_predict!=valid)
+# 0.228666
+importance(rf_customers)
+varImpPlot(rf_customers)
+
+# use entire data set 
+# BEST SO FAR
+rf_customers = randomForest(Churn~Contract+MonthlyCharges+SeniorCitizen+tenure+Dependents, data=all_customers, subset=all_train , mtry=5, importance=TRUE)
+rf_predict = predict(rf_customers, newdata=all_customers[-all_train,])
+rf_error = mean(rf_predict!=all_valid)
+# 0.2084367
+importance(rf_customers)
+varImpPlot(rf_customers)
+
+# use entire data set
+rf_customers = randomForest(Churn~Contract+MonthlyCharges+tenure, data=all_customers, subset=all_train , mtry=3, importance=TRUE)
+rf_predict = predict(rf_customers, newdata=all_customers[-all_train,])
+rf_error = mean(rf_predict!=all_valid)
+# 0.2390405
+importance(rf_customers)
+varImpPlot(rf_customers)
+
+# everything except for total???
+# rf_customers = randomForest(Churn~-customerID-TotalCharges, data=all_customers, subset=train , mtry=2, importance=TRUE)
+# rf_predict = predict(rf_customers, newdata=customers[-train,])
+# rf_error = mean(rf_predict!=customers_valid)
+# 0.2390405
+# importance(rf_customers)
+# varImpPlot(rf_customers)
